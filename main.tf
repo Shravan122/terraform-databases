20a120402b87400de5bbb55b7a57a94f30937bc2
Shravan <shravan.kumbar@gmail.com>
2022-10-28 17:14:54 +0530

    Added

--- main.tf
+++ main.tf
@@ -3,9 +3,14 @@ module "mongodb" {
   ENV                 = var.ENV
 }
 
-//module "redis" {
-  //source              = "./vendor/modules/redis"
-  //ENV                 = var.ENV
-//}
+module "redis" {
+  source              = "./vendor/modules/redis"
+  ENV                 = var.ENV
+}
+
+module "mysql" {
+  source              = "./vendor/modules/mysql"
+  ENV                 = var.ENV
+}
 
  
